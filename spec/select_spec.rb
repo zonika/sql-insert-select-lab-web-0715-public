d78a99604ff1a5c7eec3b96882ab5de8029edfa3
Rose Weixel <rose.weixel@gmail.com>
2015-02-18 14:38:02 -0500

    make clear that comment must be deleted in select_spec

--- spec/select_spec.rb
+++ spec/select_spec.rb
@@ -9,42 +9,42 @@ describe 'selecting data' do
 
   it 'selects all of the cats names' do 
     sql = <<-SQL
-    # write your sql statement here
+    # write your sql statement here and delete this comment
     SQL
     expect(@db.execute(sql)).to eq([["Maru"],["Nala"],["Grumpy Cat"], ["Lil Bub"]])
   end
 
   it 'selects all of the cats breeds' do 
     sql = <<-SQL
-    # write your sql statement here
+    # write your sql statement here and delete this comment
     SQL
     expect(@db.execute(sql)).to eq([["Scottish Fold"], ["Siamese"], ["Ragdoll"], ["Dwarf"]])
   end
 
   it 'selects all of the female cats and returns their names' do
     sql = <<-SQL
-    # write your sql statement here
+    # write your sql statement here and delete this comment
     SQL
     expect(@db.execute(sql)).to eq([["Nala"], ["Grumpy Cat"], ["Lil Bub"]])
   end
 
   it 'selects all of the declawed cats and returns their names' do 
     sql = <<-SQL
-    # write your sql statement here
+    # write your sql statement here and delete this comment
     SQL
     expect(@db.execute(sql)).to eq([["Grumpy Cat"], ["Lil Bub"]])
   end
 
   it 'selects all the friendly cats and returns their names' do 
     sql = <<-SQL
-    # write your sql statement here
+    # write your sql statement here and delete this comment
     SQL
     expect(@db.execute(sql)).to eq([["Grumpy Cat"], ["Lil Bub"]])
   end
 
   it 'selects all of the cats over 2 years old and returns their names and ages' do 
     sql = <<-SQL
-    # write your sql statement here
+    # write your sql statement here and delete this comment
     SQL
     expect(@db.execute(sql)).to eq([["Maru", 6], ["Nala", 3], ["Lil Bub", 3]])
   end
