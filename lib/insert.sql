fe1bef541caedf2b0bf148063dedc487c26fb179
zonika <zchodosh@conncoll.edu>
2015-08-14 09:06:53 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats (name, age, breed, gender, temperment, declawed)
+VALUES ('Maru', 6, 'Scottish Fold', 'M', 'playful', 0),
+('Nala', 3, 'Siamese', 'F', 'diva', 0),
+('Grumpy Cat', 2, 'Ragdoll', 'F', 'friendly', 1),
+('Lil Bub', 3, 'Dwarf', 'F', 'friendly', 1);
+
+SELECT name FROM cats;
--- spec/select_spec.rb
+++ spec/select_spec.rb
@@ -1,4 +1,4 @@
-describe 'selecting data' do 
+describe 'selecting data' do
   before do
     @db = SQLite3::Database.new(':memory:')
     SQLRunner.make_methods
@@ -7,44 +7,44 @@ describe 'selecting data' do
     @sql_runner.execute_sql_insert
   end
 
-  it 'selects all of the cats names' do 
+  it 'selects all of the cats names' do
     sql = <<-SQL
-    # write your sql statement here and delete this comment
+      SELECT name FROM cats;
     SQL
     expect(@db.execute(sql)).to eq([["Maru"],["Nala"],["Grumpy Cat"], ["Lil Bub"]])
   end
 
-  it 'selects all of the cats breeds' do 
+  it 'selects all of the cats breeds' do
     sql = <<-SQL
-    # write your sql statement here and delete this comment
+      SELECT breed FROM cats;
     SQL
     expect(@db.execute(sql)).to eq([["Scottish Fold"], ["Siamese"], ["Ragdoll"], ["Dwarf"]])
   end
 
   it 'selects all of the female cats and returns their names' do
     sql = <<-SQL
-    # write your sql statement here and delete this comment
+      SELECT name FROM cats WHERE gender = "F"
     SQL
     expect(@db.execute(sql)).to eq([["Nala"], ["Grumpy Cat"], ["Lil Bub"]])
   end
 
-  it 'selects all of the declawed cats and returns their names' do 
+  it 'selects all of the declawed cats and returns their names' do
     sql = <<-SQL
-    # write your sql statement here and delete this comment
+      SELECT name FROM cats WHERE declawed = 1
     SQL
     expect(@db.execute(sql)).to eq([["Grumpy Cat"], ["Lil Bub"]])
   end
 
-  it 'selects all the friendly cats and returns their names' do 
+  it 'selects all the friendly cats and returns their names' do
     sql = <<-SQL
-    # write your sql statement here and delete this comment
+      SELECT name FROM cats WHERE temperment = "friendly"
     SQL
     expect(@db.execute(sql)).to eq([["Grumpy Cat"], ["Lil Bub"]])
   end
 
-  it 'selects all of the cats over 2 years old and returns their names and ages' do 
+  it 'selects all of the cats over 2 years old and returns their names and ages' do
     sql = <<-SQL
-    # write your sql statement here and delete this comment
+      SELECT name, age FROM cats WHERE age > 2
     SQL
     expect(@db.execute(sql)).to eq([["Maru", 6], ["Nala", 3], ["Lil Bub", 3]])
   end
